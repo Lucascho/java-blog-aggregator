1dbd7f3d8d9dfa5fcd213baac947f4cf38dc8bbc
Lucas <Lucas@DESKTOP-QRD2GDK.hinet.net>
2017-12-13 00:20:05 +0800

    lesson 29 fix lazy 1213

--- src/main/java/cz/jiripinkas/jba/controlle/UserController.java
+++ src/main/java/cz/jiripinkas/jba/controlle/UserController.java
@@ -1,8 +1,5 @@
 package cz.jiripinkas.jba.controlle;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
-
-import java.lang.reflect.Constructor;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,48 +10,64 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import cz.jiripinkas.jba.entity.Blog;
 import cz.jiripinkas.jba.entity.User;
+import cz.jiripinkas.jba.service.BlogService;
 import cz.jiripinkas.jba.service.UserService;
 
 @Controller
 public class UserController {
-	
+
 	@Autowired
 	private UserService userService;
-	
+
+	@Autowired
+	private BlogService blogService;
+
 	@ModelAttribute("user")
- 	public User construct() {
- 		return new User();
- 	}
-	
- 	@RequestMapping("/users")
-  	public String users(Model model) {
-  		model.addAttribute("users", userService.findAll());
-  		return "users";
-  	}
-	
- 	@RequestMapping("/users/{id}")
-  	public String detail(Model model, @PathVariable int id) {
-  		model.addAttribute("user", userService.findOne(id));
-  		return "user-detail";
-  	}
-	
+	public User constructUser() {
+		return new User();
+	}
+
+	@ModelAttribute("blog")
+	public Blog constructBlog() {
+		return new Blog();
+	}
+
+	@RequestMapping("/users")
+	public String users(Model model) {
+		model.addAttribute("users", userService.findAll());
+		return "users";
+	}
+
+	@RequestMapping("/users/{id}")
+	public String detail(Model model, @PathVariable int id) {
+		model.addAttribute("user", userService.findOneWithBlogs(id));
+		return "user-detail";
+	}
+
 	@RequestMapping("/register")
- 	public String showRegistrer() {
- 		return "user-register";
- 	}
-	
-	@RequestMapping(value="/register", method=RequestMethod.POST)
+	public String showRegistrer() {
+		return "user-register";
+	}
+
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
 	public String doRegistrer(@ModelAttribute("user") User user) {
 		userService.save(user);
 		return "redirect:/register.html?success=true";
 	}
-	
+
 	@RequestMapping("/account")
-	public String account(Model model, Principal principal){
+	public String account(Model model, Principal principal) {
 		String name = principal.getName();
 		model.addAttribute("user", userService.findOneWithBlogs(name));
 		return "user-detail";
 	}
-	
+
+	@RequestMapping(value = "/account", method = RequestMethod.POST)
+	public String doAddBlog(@ModelAttribute("blog") Blog blog, Principal principal) {
+		String name = principal.getName();
+		blogService.save(blog, name);
+		return "redirect:/account.html";
+	}
 }
--- src/main/java/cz/jiripinkas/jba/service/InitDbService.java
+++ src/main/java/cz/jiripinkas/jba/service/InitDbService.java
@@ -36,16 +36,17 @@ public class InitDbService {
 	@Autowired
 	private ItemRepository itemRepository;
 	
- 	@PostConstruct
- 	public void init() {
- 		Role roleUser = new Role();
- 		roleUser.setName("ROLE_USER");
- 		roleRepository.save(roleUser);
-		
- 		Role roleAdmin = new Role();
- 		roleAdmin.setName("ROLE_ADMIN");
-  		roleRepository.save(roleAdmin);
-		
+
+	@PostConstruct
+	public void init() {
+		Role roleUser = new Role();
+		roleUser.setName("ROLE_USER");
+		roleRepository.save(roleUser);
+
+		Role roleAdmin = new Role();
+		roleAdmin.setName("ROLE_ADMIN");
+		roleRepository.save(roleAdmin);
+
 		User userAdmin = new User();
 		userAdmin.setEnabled(true);
 		userAdmin.setName("admin");
@@ -56,29 +57,26 @@ public class InitDbService {
 		roles.add(roleUser);
 		userAdmin.setRoles(roles);
 		userRepository.save(userAdmin);
-		
+
 		Blog blogJavavids = new Blog();
-		blogJavavids.setName("Javavids");
+		blogJavavids.setName("JavaVids");
 		blogJavavids.setUrl("http://feeds.feedburner.com/javavids?format=xml");
 		blogJavavids.setUser(userAdmin);
 		blogRepository.save(blogJavavids);
-		
+
 		Item item1 = new Item();
 		item1.setBlog(blogJavavids);
 		item1.setTitle("first");
 		item1.setLink("http://www.javavids.com");
 		item1.setPublishedDate(new Date());
 		itemRepository.save(item1);
-		
+
 		Item item2 = new Item();
 		item2.setBlog(blogJavavids);
 		item2.setTitle("second");
 		item2.setLink("http://www.javavids.com");
 		item2.setPublishedDate(new Date());
 		itemRepository.save(item2);
-		
-		
-		
+
 	}
-	
 }
